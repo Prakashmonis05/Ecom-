b351111f2ec4b06df365e5eb7acfb63b8bb29941
Prakash Monis <Prakashmonis06@gmail.com>
2026-09-06 17:18:10 +0530

    added api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000",
-   
+    baseURL: import.meta.env.VITE_API_URL,
+
 });
 
 api.interceptors.request.use(
